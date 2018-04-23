a0aed94fb4a2f75ef1fb03158b196bd26f22fe93
Talfaran <tal1faran@gmail.com>
2018-04-23 10:41:01 +0300

    blackand white garbi

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 var idCount = 0
-var id;
-userInputId = '';
 var posts = [];
-// var commants = [];
 var addPost = function () {
     var post = {
         text: $('#post-name').val(),
@@ -15,29 +12,36 @@ var addPost = function () {
 }
 
 var addCommants = function () {
-    var commant = $(this).siblings('.commant-input').val(),
+    var commant = {
+        CommentText: $(this).siblings('.commant-input').val(),
+        userName: $(this).siblings('.commant-username').val()
+    }
+
         id = $(this).closest('li').data().id;
     for (var i = 0; i < posts.length; i++) {
         if (id === posts[i].id) {
             posts[i].commants.push(commant)
-            for (let j = 0; j < posts[i].commants.length; j++) {
-                alert(posts[i].commants[j]);
-            }
+       
         }
     }
-
 }
 
-var renderPost = function () {
-    $('ul').find('li').remove();
-    for (let i = 0; i < posts.length; i++) {
-        $('ul').append('<li>' + posts[i].text + " " + '<button type="button" class="remove">' + 'REMOVE' + '</button>' + '<button type="button" class="commant">' + 'COMMANT' + '</button>' + '<input = text class = commant-input>' + '</li>');
-        $('li').attr('data-id', posts[i].id);
-        $('li').attr('id', posts[i].id)
-        $('li').find('input').attr('data-commant', posts[i].id);
-        $('li').find('.remove').attr('id', posts[i].id)
-    }
-}
+$('.add-post').click(addPost)
+$('ul').on('click', '.remove', function () {
+    id = $(this).closest('li').data().id;
+    for (var i = 0; i < posts.length; i++)
+        if (id === posts[i].id) {
+            posts.splice(i, 1);
+        }
+    renderPost()
+});
+
+$('ul').on('click', '.commant', addCommants)
+
+
+
+
+
 // var renderCommants = function () {
 //   userInputId = $(this).closest('input').data().commant;
 // for (let i = 0; i < commants.length; i++) {
@@ -48,22 +52,11 @@ var renderPost = function () {
 // }
 //}
 
-$('.add-post').click(addPost)
-$('ul').on('click', '.remove', function () {
-    id = $(this).closest('li').data().id;
-    for (var i = 0; i < posts.length; i++)
-        if (id === posts[i].id) {
-            posts.splice(i, 1);
-        }
-    renderPost()
-
-
-
-});
-
-$('ul').on('click', '.commant', addCommants)
 
 
       //add the render comments in the render post
 
 
+    //   for (let j = 0; j < posts[i].commants.length; j++) {
+    //     alert(posts[i].commants[j]);
+    // }
